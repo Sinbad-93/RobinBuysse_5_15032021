b11ee114037b9254eb0a584c1f845dd29da37819
Sinbad <rob1_93@hotmail.fr>
2021-03-15 15:34:33 +0100

    Building add to bucket product

--- docs/front_end/bucket.js
+++ docs/front_end/bucket.js
@@ -1,7 +1,7 @@
 var product = document.querySelector('.product');
 var requestData = [];
-var savedProducts = [];
-var id = localStorage.getItem('number');
+
+id = localStorage.getItem('number');
 
 var title = document.querySelector('.title');
 var price = document.querySelector('.price');
@@ -9,7 +9,6 @@ var description = document.querySelector('.description');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var color3 = document.querySelector('.color3');
-var addToBucket = document.querySelector('.addToBucket');
 
 console.log(title);
 
@@ -32,50 +31,6 @@ const data = await response.json()
     color3.textContent = requestData['colors'][2];	
 
      });}
-
-function addProductToBucket(product_id){
-    savedProducts.push(product_id);
-    localStorage.setItem('picked', savedProducts);
-    console.log(localStorage);
-    console.log(localStorage.getItem('picked'));
-}
   
+
 getOneProduct(id);
-addProductToBucket(id);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var uRL = window.location.href;
-console.log(uRL);*/
-
-/*function getParameterByName(name)
-{
- name = name.replace(/[[]/, [).replace(/[]]/, ]);
- var regexS = [?&] + name + =([^&#]*);
- var regex = new RegExp(regexS);
- var results = regex.exec(window.location.search);
- if(results == null)
- return ;
- else
- return decodeURIComponent(results[1].replace(/+/g, ));
-}
-
-getParameterByName(uRL);*/
