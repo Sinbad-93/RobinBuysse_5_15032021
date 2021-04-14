77cccf5e406f0cc2f44bc42874d563d58c0e9106
Sinbad <rob1_93@hotmail.fr>
2021-04-14 11:10:46 +0200

    implenter l'appel api également sur le premiere page

--- docs/front_end/script.js
+++ docs/front_end/script.js
@@ -1,13 +1,46 @@
-/*DOM classe des articles*/
-/*var productsPicturesNames = [];
-var productsPictures = [];*/
+/* se relier aux éléments du DOM*/
+var picture = document.querySelector('.teddy');
+var teddyName = document.querySelector('.teddyName');
+var price = document.querySelector('.teddyPrice');
+const container = document.querySelector('.picture_container');
+/*initialiser variable pour les futurs données*/
+let teddyData = "";
+let testo;
+async function getAllProduct(){
+    await fetch('http://localhost:3000/api/teddies/')
+  .then( response => {
+const data = response.json()
+  .then(data => showAllTeddies(data));});}
 
-/* générer une Array reliée aux éléments du DOM
-function getPageProducts(){
-for(var i=0; i<5; i++){
-  productsPicturesNames.push('.teddy_'+(i+1));
-  productsPictures[i] = document.querySelector(productsPicturesNames[i]);
-}};*/
+/*afficher les données sur le DOM*/
+function showAllTeddies(data){
+  /*gérer le cas du panier vide*/
+if (data.length === 0) {
+  container.innerHTML = "<span>Accès aux produits impossible</span>";
+return;}
+
+for (i in data){
+/*insérer les données dans la variable en bouclant sur chaque produit*/
+  teddyData = `<div class='picture shadow_picture'><a href='product.html'>
+  <img id='teddy${i+1+'_'+data[i]['_id']}' 
+  class='teddy' onclick='getId(this.id);'
+   src='${data[i]['imageUrl']}' alt=''></a><p> 
+  <span class='teddyName'>${data[i]['name']}</span>
+  <span class='teddyPrice'>${data[i]['price']}</span>
+  </p></div>`;
+
+/*insérer la variable dans un élément du DOM pour afficher données */
+container.innerHTML += teddyData;}
+
+  /*for (var i = 0; i < 5; i++){
+  var path = data[i]['imageUrl'];
+  picture.src = path;
+  teddyName.textContent = data[i]['name'];
+  price.textContent = data[i]['price'] + ' ¥';
+  }*/
+}
+
+
 
 /*fonction déclanchée sur le onclick html, permet de mémoriser sur quel produit l'on a cliqué */
 function getId(number)
@@ -20,4 +53,5 @@ localStorage.setItem('productId',separateString[1]);
 localStorage.setItem(separateString[0],separateString[1]);
 }
 
+getAllProduct();
 /*getPageProducts()*/
